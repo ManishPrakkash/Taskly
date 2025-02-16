b68dc6d7cdf6b3eee4ca1f0f7b0479829093dc76
ManishPrakkash <manishprakkash@gmail.com>
2025-02-16 17:19:05 +0000

    components added

---
+++ taskly/src/components/Header.js
@@ -0,0 +1,11 @@
+import Logo from "../assets/Logo.png"
+
+export const Header = () => {
+  return (
+    <header>
+        <div className="logo">
+            <img src="" alt="" />
+        </div>
+    </header>
+  )
+}

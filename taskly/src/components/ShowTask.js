0c0c6238b5b37ac97678798565b434490ab4c060
ManishPrakkash <manishprakkash@gmail.com>
2025-02-16 17:45:19 +0000

    show task structure

--- taskly/src/components/ShowTask.js
+++ taskly/src/components/ShowTask.js
@@ -2,6 +2,24 @@ import React from 'react'
 
 export const ShowTask = () => {
   return (
-    <></>
+    <section className="showTask">
+        <div className="head">
+            <div> 
+               <span className="title">Todo</span>
+                <span className="count">0</span>
+            </div>
+            <button className="clearAll">Clear All</button>
+        </div>
+        <ul>
+            <li>
+                <p>
+                    <span></span>
+                    <span></span>
+                </p>
+                <i></i>
+                <i></i>
+            </li>
+        </ul>
+    </section>
   )
 }

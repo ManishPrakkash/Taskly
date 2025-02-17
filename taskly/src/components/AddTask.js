fd0ebcbb15d480d1dea64b9a8b0fe31cda30e239
ManishPrakkash <manishprakkash@gmail.com>
2025-02-17 18:56:53 +0000

    changes

--- taskly/src/components/AddTask.js
+++ taskly/src/components/AddTask.js
@@ -1,4 +1,4 @@
-export const AddTask = ({tasklist,setTasklist}) => {
+export const AddTask = ({tasklist,setTasklist,task,setTask}) => {
 
   const handleSubmit =(e)=>{
     e.preventDefault();
@@ -7,15 +7,16 @@ export const AddTask = ({tasklist,setTasklist}) => {
     const newTask = {
       id:date.getTime(),
       name:e.target.task.value,
-      time:`${date.toLocaleTimeString()} ${date.toLocaleDateString}`
+      time:`${date.toLocaleTimeString()} ${date.toLocaleDateString()}`
     }
-    setTasklist([...taskList,newTask]);
+    setTasklist([...tasklist,newTask]);
+    e.target.task.value ="";
   }
   
   return (
   <section className="addTask">
     <form onSubmit={handleSubmit}>
-        <input type="text" name="task" autoComplete="off" placeholder="add task" maxLength={25}/>
+        <input value={task.name} type="text" name="task" autoComplete="off" placeholder="add task" maxLength={25}/>
         <button type="submit">Add</button>
     </form>
   </section>

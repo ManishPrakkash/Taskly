e2a923c73e016d616d319411961b7c83f9ae3850
ManishPrakkash <manishprakkash@gmail.com>
2025-02-17 18:56:34 +0000

    components changes

--- taskly/src/App.js
+++ taskly/src/App.js
@@ -5,12 +5,21 @@ import { ShowTask } from './components/ShowTask';
 import { useState } from "react";
 function App() {
   const [tasklist, setTasklist]= useState([]);
-  
+  const [task, setTask]= useState({});
  return (
   <div className="app">
     <Header />
-    <AddTask tasklist={tasklist} setTasklist={setTasklist}/>
-    <ShowTask tasklist={tasklist} setTasklist={setTasklist}/>
+    <AddTask 
+    tasklist={tasklist} 
+    setTasklist={setTasklist}
+    task={task}
+    setTask={setTask}
+  />
+    <ShowTask 
+    tasklist={tasklist} 
+    setTasklist={setTasklist}
+    task={task}
+    setTask={setTask}/>
   </div>
  );
 }

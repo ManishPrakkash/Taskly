376ba4dcf17afab70e209ee5ba38460dfb90c8fb
ManishPrakkash <manishprakkash@gmail.com>
2025-02-18 18:21:56 +0000

    theme added

--- taskly/src/components/Header.js
+++ taskly/src/components/Header.js
@@ -8,12 +8,12 @@ export const Header = () => {
             <span>Taskly</span>
         </div>
         <div className="themeSelector">
-            <span className="light active"></span>
-            <span className="medium"></span>
-            <span className="dark"></span>
-            <span className="gOne"></span>
-            <span className="gTwo"></span>
-            <span className="gThree"></span>
+            <span onClick={()=>setTheme("light")} className={theme === "light" ? "light activeTheme":"light"}></span>
+            <span onClick={()=>setTheme("medium")} className={theme === "medium" ? "medium activeTheme":"medium"}></span>
+            <span onClick={()=>setTheme("dark")} className={theme === "dark" ? "dark activeTheme":"dark"}></span>
+            <span onClick={()=>setTheme("gOne")} className={theme === "gOne" ? "gOne activeTheme":"gOne"}></span>
+            <span onClick={()=>setTheme("gTwo")} className={theme === "gTwo" ? "gTwo activeTheme":"gTwo"}></span>
+            <span onClick={()=>setTheme("gThree")} className={theme === "gThree" ? "gThree activeTheme":"gThree"}></span>
         </div>
     </header>
   )

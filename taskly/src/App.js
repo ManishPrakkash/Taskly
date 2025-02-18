9ccab7f3067384233b99e03dc104b453b6dafa86
ManishPrakkash <manishprakkash@gmail.com>
2025-02-18 17:20:38 +0000

    daata added to localstorage

--- taskly/src/App.js
+++ taskly/src/App.js
@@ -2,10 +2,28 @@ import './App.css';
 import { AddTask } from './components/AddTask';
 import { Header } from './components/Header';
 import { ShowTask } from './components/ShowTask';
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+
 function App() {
-  const [tasklist, setTasklist]= useState([]);
-  const [task, setTask]= useState({});
+
+  const [tasklist, setTasklist] = useState(() => {
+    try {
+      const savedTasklist = localStorage.getItem("tasklist");
+      return savedTasklist ? JSON.parse(savedTasklist) : [];
+    } 
+    catch (error) {
+      console.error("Failed to parse tasklist from localStorage", error);
+      return [];
+    }
+  });
+
+  const [task, setTask] = useState({});
+
+  useEffect(() => {
+    localStorage.setItem("tasklist", JSON.stringify(tasklist));
+  }, [tasklist]); 
+
  return (
   <div className="app">
     <Header />

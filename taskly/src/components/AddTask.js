06f9895a4d1a5955c5ecf6345ed536e0eab9e8ce
ManishPrakkash <manishprakkash@gmail.com>
2025-02-18 14:34:00 +0000

    value adde to input when edit

--- taskly/src/components/AddTask.js
+++ taskly/src/components/AddTask.js
@@ -3,20 +3,31 @@ export const AddTask = ({tasklist,setTasklist,task,setTask}) => {
   const handleSubmit =(e)=>{
     e.preventDefault();
 
-    const date = new Date();
+    if(task.id){
+      const date = new Date();
+      const updatedTasklist=tasklist.map((todo)=>(
+        todo.id === task.id ? 
+        {id:task.id, name:e.target.task.value,time:`${date.toLocaleTimeString()} ${date.toLocaleDateString()}`} : todo
+      ));
+      setTasklist(updatedTasklist);
+      setTask([]);
+    }
+    else{
+       const date = new Date();
     const newTask = {
       id:date.getTime(),
-      name:e.target.task.value,
+      name:task.name,
       time:`${date.toLocaleTimeString()} ${date.toLocaleDateString()}`
     }
     setTasklist([...tasklist,newTask]);
     e.target.task.value ="";
+    }
   }
   
   return (
   <section className="addTask">
     <form onSubmit={handleSubmit}>
-        <input value={task.name} type="text" name="task" autoComplete="off" placeholder="add task" maxLength={25}/>
+        <input value={task.name} type="text" name="task" autoComplete="off" placeholder="add task" maxLength={25} onChange={e => setTask({...task,name:e.target.value})}/>
         <button type="submit">Add</button>
     </form>
   </section>
